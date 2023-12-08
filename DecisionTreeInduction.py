7cce77364ead75088d3045ce00d1f020fa6bfa6a
Labeeb-Abrar <labeeb.abrar@stonybrook.edu>
2023-12-08 10:23:30 -0500

    works

--- DecisionTreeInduction.py
+++ DecisionTreeInduction.py
@@ -7,20 +7,24 @@ from pprint import pprint
 from ucimlrepo import fetch_ucirepo
 
 def entropy(data):
-    cnt = Counter(data) # count the number of each instance
-    p = [x / len(data) for x in cnt.values()]
+    hashed_data = Counter(data) # dictionary of data, indiced by class
+    entropy_list= []
+    for x in hashed_data.values():
+        p = (x / len(data))
+        entropy_list.append(-p * math.log(p, 2))
 
-    return -sum([pi * math.log(pi) for pi in p]) # Info(data) = -sum( pi*log(pi) )
+    return sum(entropy_list) # Info(data) = -sum( pi*log(pi) )
 
 def info_gain(d: pd.DataFrame, attr, target_attr):
     # Split Data by Possible Vals of Attribute:
     d_split = d.groupby(attr)
-
+    # print(d_split.indices)
     # Calculate Entropy for Target Attribute, as well as Proportion of Obs in Each Data-Split:
     entropies = []
     ratio = []
 
     for group_name, group_data in d_split:
+        # print(group_data[target_attr])
         group_entropy = entropy(group_data[target_attr])    # log(|dj|/|d|)
         entropies.append(group_entropy) # Info(D)
         
@@ -38,46 +42,63 @@ def info_gain(d: pd.DataFrame, attr, target_attr):
     old_entropy = entropy(d[target_attr])
     return old_entropy - new_entropy
 
-
-#   generate decision tree
 def generate_DT(D: pd.DataFrame, attr_list, target_attr, majority=None):
-    N = Node(data=None, children={})
-    classlist_in_d = Counter(D)
+    # Tally target attribute:
+    classlist_in_d = Counter(x for x in D[target_attr])
 
-    #   if dataset tuple is entirely of a single class
+    # First check: Is this split of the dataset homogeneous?
     if len(classlist_in_d) == 1:
-        return N.set(attr_list[0])
-    #   if attribute list is empty (empty when all but the class that this ruleset is predicting)
-    if D.empty or not attr_list:
-        return N.set(majority)
+        return list(classlist_in_d.keys())[0]
 
-    #   attribute selection
+    # Second check: Is this split of the dataset empty?
+    # if yes, return a default value
+    if D.empty or (not attr_list):
+        return majority
+
+    # Otherwise: This dataset is ready to be divvied up!
+    # # Get Default Value for next recursive call of this function:
+    index_of_max = list(classlist_in_d.values()).index(max(classlist_in_d.values()))
+    majority_class = list(classlist_in_d.keys())[index_of_max]  # most common value of target attribute in dataset
+
+    # Choose Best Attribute to split on:
     gains = [info_gain(D, attr, target_attr) for attr in attr_list]
     index_of_max = gains.index(max(gains))  # pick one with highest gain
     split_criteria = attr_list[index_of_max]    # best splitting decision
-    print(f"best attribute: {split_criteria}")
-    N.set(split_criteria)
 
-    max_key = list(classlist_in_d.values()).index(max(classlist_in_d.values())) # get majority class in Dj
-    majority_class = list(classlist_in_d.keys())[max_key]
-    new_attr_list = attr_list.copy()
-    new_attr_list.remove(split_criteria)
+    # Create an empty tree, to be populated in a moment
+    N = {split_criteria: {}}
+    new_attr_list = []
+    for i in attr_list:
+        if i != split_criteria:
+            new_attr_list.append(i)
 
+    # recursion
     for attr, Dj in D.groupby(split_criteria):
-        newNode = generate_DT(Dj, new_attr_list, target_attr, majority_class)
-        N.append(newNode)
-    
+        subtree = generate_DT(Dj, new_attr_list, target_attr, majority_class)
+        N[split_criteria][attr] = subtree
     return N
 
+def decision_tree_induction(D: pd.DataFrame, attr_list, target_attr):
+    print("generating Decision Tree")
+    targetattr_hashed = Counter(x for x in D[attributes_list])
+    index_of_max = list(targetattr_hashed.values()).index(max(targetattr_hashed.values()))
+    majority_class = list(targetattr_hashed.keys())[index_of_max]  # most common value of target attribute in dataset
+    return generate_DT(D, attr_list, target_attr, majority_class)
 
 
 ########
 df_shroom = pd.read_csv('data/mushroom_data.csv')
+id = 73
+data = fetch_ucirepo(id=id)
+df_shroom2 = data['data']['original']
+print(df_shroom)
 
-# Get Predictor Names (all but 'class')
-attribute_names = list(df_shroom.columns)
-attribute_names.remove('class')
-print(df_shroom['class'])
+# Get class to predict
+predicting_class = "class"
+attributes_list = list(df_shroom.columns)
+attributes_list.remove(predicting_class)
 
-dtree = generate_DT(df_shroom, attribute_names, "class")
+total_rows = int(df_shroom.shape[0] * .8)
+training_data = df_shroom.iloc[1:total_rows]  # 80% of data as training data
+dtree = decision_tree_induction(df_shroom, attributes_list, predicting_class)
 pprint(dtree)
